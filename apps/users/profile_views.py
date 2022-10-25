735584751ebd2281315a3bfb774d7e3c78b2b3a9
artem <kalaficzkij@gmail.com>
2022-10-25 13:38:04 +0300

    added user dependencies, correct user views

--- apps/users/profile_views.py
+++ apps/users/profile_views.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import UUID4
 
-from apps.users.manager import get_user_manager
+from apps.users.dependencies import get_user_manager
+from apps.users.manager import UserManager
 
 profile_router = APIRouter()
 
 
 @profile_router.get("/")
-async def get_profile(id: UUID4):
-    manager = await get_user_manager()
-    user = await manager.get_user(id)
+async def get_profile(
+    user_id: UUID4,
+    user_manager: UserManager = Depends(get_user_manager),
+):
+    user = await user_manager.get_user(user_id)
     return user
 
 
 @profile_router.put("/")
-async def update_profile(id: UUID4):
-    manager = await get_user_manager()
-    user = await manager.get_user(id)
+async def update_profile(
+    user_id: UUID4,
+    user_manager: UserManager = Depends(get_user_manager),
+):
+    user = await user_manager.get_user(user_id)
     return user
