67db9e18b46df71eec98e4915e774e00b1e89317
Bogdan1726 <bogdan24ro@gmail.com>
2022-11-09 21:05:35 +0200

    added changes in app, socket_server and html templates

--- apps/socketio_app/socket_server.py
+++ apps/socketio_app/socket_server.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 import socketio
 
-# mgr = socketio.AsyncAioPikaManager('amqp://guest:guest@localhost/', 'new_blocks', logger=True)
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
-# app = web.Application()
-# sio.attach(app)
 
 
 @sio.event
@@ -12,26 +9,12 @@ async def connect(sid, environ, auth):
     print("connect", sid)
 
 
-@sio.event
-async def new_transaction(sid, data):
-    print(data)
-
-
-@sio.event
-async def update_balance(sid, data):
-    print(data)
-
-
-@sio.event
-async def new_blocks(sid, data):
-    print(sid)
-    print(data)
+@sio.event()
+async def new_message(sid, data):
+    print("message ", data)
+    await sio.emit("update_message", data)
 
 
 @sio.event
 async def disconnect(sid):
-    print("disconnect", sid)
-
-
-# if __name__ == '__main__':
-#     web.run_app(app, host='localhost', port=5000)
+    print("disconnect ", sid)
